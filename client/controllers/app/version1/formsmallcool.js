7e4f5806240618b41728d312acbc2b1184aafcc3
Natalia Savchak <nsavchak@softjourn.com>
2017-07-24 13:13:26 +0300

    test version

---
+++ client/controllers/app/version1/formsmallcool.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_version1_formsmallcool', app_version1_formsmallcool);
+function app_version1_formsmallcool($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/states.js
@@ -0,0 +1,46 @@
+angular.module('app').config(function ($stateProvider) {
+    'use strict';
+    $stateProvider.state('app', {
+        abstract: true,
+        controller: 'app',
+        templateProvider: function (app) {
+            return app.templateProvider('app');
+        }
+    }).state('app.blank', {
+        views: {
+            app: {
+                controller: 'app_blank',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.blank');
+                }
+            }
+        }
+    }).state('app.menu', {
+        views: {
+            app: {
+                controller: 'app_menu',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.menu');
+                }
+            }
+        }
+    }).state('app.multiselect', {
+        views: {
+            app: {
+                controller: 'app_multiselect',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.multiselect');
+                }
+            }
+        }
+    }).state('app.version1/formsmallcool', {
+        views: {
+            app: {
+                controller: 'app_version1_formsmallcool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.version1.formsmallcool');
+                }
+            }
+        }
+    });
+});
