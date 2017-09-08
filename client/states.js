133161f32594d6b05be281af8b131095631228e2
test <test@test.com>
2017-09-08 13:24:55 +0000

    [Studio] Add screen "Home-Cool"

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.homecool', {
+        views: {
+            app: {
+                controller: 'app_homecool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.homecool');
+                }
+            }
+        }
     });
 });
